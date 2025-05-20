b7715244ee3413a3c8192cebbdfe19c375376f10
Eeka Kivivuori <shilliwowi@gmail.com>
2025-05-20 14:09:42 +0300

    Remove style.css

--- packages/@luminx/core/src/Icon/types.ts
+++ packages/@luminx/core/src/Icon/types.ts
@@ -3,22 +3,16 @@ import { Radius, Shadow } from "../_theme";
 
 export type IconSize = "xs" | "sm" | "md" | "lg" | "xl";
 
-export interface IconGradient {
-    from: string;
-    to: string;
-    deg?: number;
-}
-
 export interface IconProps extends HTMLAttributes<HTMLDivElement> {
-    children?: ReactNode;
-    size?: IconSize;
-    radius?: Radius;
-    shadow?: Shadow;
-    classNames?: IconClassNames;
-    component?: ElementType;
+  children?: ReactNode;
+  size?: IconSize;
+  radius?: Radius;
+  shadow?: Shadow;
+  classNames?: IconClassNames;
+  component?: ElementType;
 }
 
 export interface IconClassNames {
-    root?: string;
-    icon?: string;
+  root?: string;
+  icon?: string;
 }
