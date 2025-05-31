c68d71e3f70b4e7304e76d104e4b3506cb5e05b1
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-05-31 16:39:53 +0300

    Fixed spacing issues

--- packages/@luminx/core/src/Avatar/AvatarGroup.tsx
+++ packages/@luminx/core/src/Avatar/AvatarGroup.tsx
@@ -1,3 +1,4 @@
+import { Children, cloneElement, isValidElement, ReactElement } from "react";
 import { cx } from "../_theme";
 
 export interface AvatarGroupProps {
@@ -11,14 +12,29 @@ export const AvatarGroup = ({
     spacing = -8,
     className
 }: AvatarGroupProps) => {
-    const marginOffset = Math.abs(spacing);
+    const style: React.CSSProperties = {
+        ["--avatar-group-margin-offset" as any]: `${spacing}px`
+    };
 
     return (
-        <div
-            className={cx("flex items-center", className)}
-            style={{ marginLeft: marginOffset }}
-        >
-            {children}
+        <div className={cx("flex items-center", className)} style={style}>
+            {Children.map(children, (child, index) => {
+                if (isValidElement(child)) {
+                    const element = child as ReactElement<{
+                        className?: string;
+                    }>;
+
+                    return cloneElement(element, {
+                        className: cx(
+                            index !== 0
+                                ? "ml-[var(--avatar-group-margin-offset)]"
+                                : "",
+                            element.props.className
+                        )
+                    });
+                }
+                return child;
+            })}
         </div>
     );
 };
