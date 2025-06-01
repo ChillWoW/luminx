db98a745f2968ccb76dcf763914e5dd1f0cd56d1
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-06-01 23:40:16 +0300

    Tried some stupid things

--- packages/@luminx/dates/tailwind.config.js
+++ packages/@luminx/dates/tailwind.config.js
@@ -6,17 +6,6 @@ module.exports = {
             colors: {}
         }
     },
-    safelist: [
-        "grid-cols-1",
-        "grid-cols-2",
-        "grid-cols-3",
-        "grid-cols-4",
-        "grid-cols-5",
-        "grid-cols-6",
-        "grid-cols-7",
-        "grid",
-        "gap-1",
-        "gap-2"
-    ],
+    safelist: ["w-80"],
     plugins: [require("tailwind-scrollbar")({ nocompatible: true })]
 };
