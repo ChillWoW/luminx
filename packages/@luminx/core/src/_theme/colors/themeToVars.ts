8f9a2bd527555dfacc089a25205d186e89e4e9f4
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2025-06-17 23:57:23 +0300

    Dark colors in components to be one lighter

--- packages/@luminx/core/src/_theme/colors/themeToVars.ts
+++ packages/@luminx/core/src/_theme/colors/themeToVars.ts
@@ -11,14 +11,14 @@ interface ThemeVars {
 const getThemeColors = () => {
     const vars: ThemeVars = {};
 
-    vars[`${DEFAULT_TEMPLATE}-dark-background`] = colors.dark[7];
-    vars[`${DEFAULT_TEMPLATE}-dark-background-hover`] = colors.dark[6];
-    vars[`${DEFAULT_TEMPLATE}-dark-border`] = colors.dark[5];
-    vars[`${DEFAULT_TEMPLATE}-dark-border-hover`] = colors.dark[4];
+    vars[`${DEFAULT_TEMPLATE}-dark-background`] = colors.dark[6];
+    vars[`${DEFAULT_TEMPLATE}-dark-background-hover`] = colors.dark[5];
+    vars[`${DEFAULT_TEMPLATE}-dark-border`] = colors.dark[4];
+    vars[`${DEFAULT_TEMPLATE}-dark-border-hover`] = colors.dark[3];
     vars[`${DEFAULT_TEMPLATE}-dark-text`] = colors.white;
-    vars[`${DEFAULT_TEMPLATE}-dark-hint`] = colors.dark[2];
-    vars[`${DEFAULT_TEMPLATE}-dark-placeholder`] = colors.dark[2];
-    vars[`${DEFAULT_TEMPLATE}-dark-section`] = colors.dark[1];
+    vars[`${DEFAULT_TEMPLATE}-dark-hint`] = colors.dark[1];
+    vars[`${DEFAULT_TEMPLATE}-dark-placeholder`] = colors.dark[1];
+    vars[`${DEFAULT_TEMPLATE}-dark-section`] = colors.dark[0];
 
     vars[`${DEFAULT_TEMPLATE}-light-background`] = colors.light[3];
     vars[`${DEFAULT_TEMPLATE}-light-background-hover`] = colors.light[4];
